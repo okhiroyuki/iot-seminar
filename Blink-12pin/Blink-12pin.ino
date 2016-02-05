1e40863666e8abaceb129a4f6cf182b6a37c6c02
hiroyuki okada <ok.okada.hiroyuki@gmail.com>
2016-02-06 08:47:06 +0900

    回路図を追加

--- Blink-12pin/Blink-12pin.ino
+++ Blink-12pin/Blink-12pin.ino
@@ -18,16 +18,12 @@
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(12, OUTPUT);
-  pinMode(3,INPUT_PULLUP);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  if(digitalRead(3) == LOW){
-    digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);              // wait for a second
-    digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);              // wait for a second
-  }
-  digitalWrite(12,LOW);  
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
